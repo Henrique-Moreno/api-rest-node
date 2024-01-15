0dee782f76502a6463a7be6da25820b5596e8cff
Henrique Moreno <henriqueprofissional26@gmail.com>
2024-01-14 21:16:03 -0300

    server host port

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import { app } from './app';
 import { env } from './env';
 
+const port = env.PORT;
+const host = "RENDER" in process.env ? "0.0.0.0" : "localhost";
+
 app.listen({
-  port: env.PORT,
+ // port: env.PORT,
+  host, port
 }).then(() => {
   console.log('HTTP Server Running!');
 });
